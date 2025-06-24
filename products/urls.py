cdad864194f38c05d45cf9cc708c51d3b775533a
Shruti Singh <shruti29125@gmail.com>
2025-06-24 14:32:18 +0530

    Initial commit

---
+++ products/urls.py
@@ -0,0 +1,8 @@
+from rest_framework.routers import DefaultRouter
+from .views import ProductViewSet
+from django.urls import path, include
+
+router = DefaultRouter()
+router.register(r'products', ProductViewSet)
+
+urlpatterns = router.urls
