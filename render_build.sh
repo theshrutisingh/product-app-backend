08edd62240cbcf7d702767dea76c9942f26251e5
Shruti Singh <161053949+theshrutisingh@users.noreply.github.com>
2025-06-24 15:52:46 +0530

    Update render_build.sh

--- render_build.sh
+++ render_build.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
-#!/usr/bin/env bash
 
+# Step 1: Install dependencies
 pip install -r requirements.txt
 
-
+python manage.py makemigrations
 python manage.py migrate
+
+# Step 3: Collect static files (optional but good practice)
+python manage.py collectstatic --noinput
+
+# Step 4: Run Gunicorn server
 gunicorn backend.wsgi:application
